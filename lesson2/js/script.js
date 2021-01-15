af38a510df0974d119e1a3391a3edd058394d55d
tjproduces <soloteer@gmail.com>
2021-01-15 09:42:31 -0700

    initial commit

---
+++ lesson2/js/script.js
@@ -0,0 +1,2 @@
+document.getElementById('last-modified').innerHTML = "<p> Last Updated: " + document.lastModified + "</p>"
+document.getElementById('current-year').innerHTML = new Date().getFullYear()
